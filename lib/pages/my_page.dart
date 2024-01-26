e024a113c43a106534d1cba2298616099b873061
raisu3141 <yuuta.matayoshi1413@gmail.com>
2024-01-26 22:21:29 +0900

    mainブランチの変更によりプルリクエストがクローズされてしまったため

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -24,7 +24,7 @@ class Mypage extends StatelessWidget {
     return  Scaffold(
       appBar: AppBar(
         centerTitle: true, //中央寄せの指定
-        title: const Text('Instagramだよ')
+        title: const Text('マイページ')
         
         ),
 
