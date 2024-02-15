357431a74c4069168210da376d6757a88e33c501
raisu3141 <yuuta.matayoshi1413@gmail.com>
2024-02-15 21:17:18 +0900

    timer app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,13 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'timer',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'timer'),
     );
   }
 }
@@ -32,48 +34,99 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _second = 0;
+  Timer? _timer;
+  bool _isRanning = false;
 
-  void _incrementCounter() {
-    setState(() {
+  @override
+  void initState() {
+    
+    super.initState();
 
-      _counter++;
-    });
+    
   }
 
   @override
   Widget build(BuildContext context) {
+
+    //int second = _millisecond % 10;
 
     return Scaffold(
       appBar: AppBar(
 
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // the App.build method, and use it to set our appbar.
+        centerTitle: true,
         title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          mainAxisSize: MainAxisSize.min,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '${(_second ~/ 6000).toString().padLeft(2, '0')}:${(_second ~/ 100 % 60).toString().padLeft(2, '0')}.${(_second % 100).toString().padLeft(2, '0')}',
+              style: const TextStyle(
+                fontSize: 80,
+              ),
             ),
+
+            ElevatedButton(
+              onPressed: (){
+                toggletimer();
+              },
+               child: Text(
+                _isRanning ? 'ストップ' : 'スタート',
+                style: TextStyle(
+                  color: _isRanning ? Colors.red : Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                resettimer();
+              },
+               child: const Text(
+                'リセット',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  void toggletimer(){
+    if (_isRanning){
+      _timer?.cancel();
+    } else {
+      _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        //millisecond * 10
+        _second++;
+      });
+     });
+    }
+
+    setState(() {
+      _isRanning = !_isRanning;
+    });
+    
+  }
+
+  void resettimer(){
+    _timer?.cancel();
+    setState(() {
+      _second = 0;
+      _isRanning = false;
+    });
+  }
+
 }
